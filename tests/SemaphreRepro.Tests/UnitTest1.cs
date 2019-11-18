e63f7a65def9686854ebdeb1d3e9fb2901a9bb1d
Johnny Santos <johnnyadsantos@gmail.com>
2019-11-18 12:50:24 +0100

    Use xunit test output

--- tests/SemaphreRepro.Tests/UnitTest1.cs
+++ tests/SemaphreRepro.Tests/UnitTest1.cs
@@ -1,8 +1,8 @@
-using System;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 [assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = true, MaxParallelThreads = 1)]
 
@@ -10,6 +10,13 @@ namespace SemaphoreRepro.Tests
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper output;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public async Task TestSemaphoreSlim()
         {
@@ -19,24 +26,30 @@ namespace SemaphoreRepro.Tests
             const double msMax = (double)ms * 1.5;
             const double msMin = (double)ms * (1.0 / 1.5);
 
-            Console.WriteLine("# SemaphoreSlim");
-            var semaphore = new SemaphoreSlim(timesPerPeriod, timesPerPeriod);
+            output.WriteLine("# SemaphoreSlim");
+            using var semaphore = new SemaphoreSlim(timesPerPeriod, timesPerPeriod);
             var entered = await semaphore.WaitAsync(0);
             Assert.True(entered);
             var globalSw = Stopwatch.StartNew();
             var isWaiting = false;
+            var isFinished = false;
 
             var t = new Thread(_ =>
             {
-                while (true)
+                while (!isFinished)
                 {
-                    Console.WriteLine("[0][{0}] Sleeping. ", globalSw.Elapsed.TotalMilliseconds);
+                    output.WriteLine("[0][{0}] Sleeping. ", globalSw.Elapsed.TotalMilliseconds);
+
+                    var timer = Stopwatch.StartNew();
                     Thread.Sleep(ms);
-                    Console.WriteLine("[0][{0}] Sleep done.", globalSw.Elapsed.TotalMilliseconds);
+                    timer.Stop();
+
+                    output.WriteLine("[0][{0}] Sleeped for {1}ms", globalSw.Elapsed.TotalMilliseconds, timer.Elapsed.TotalMilliseconds);
+
                     if (isWaiting)
                     {
                         semaphore.Release();
-                        Console.WriteLine("[0][{0}] Released.", globalSw.Elapsed.TotalMilliseconds);
+                        output.WriteLine("[0][{0}] Released.", globalSw.Elapsed.TotalMilliseconds);
                     }
                 }
             })
@@ -45,28 +58,35 @@ namespace SemaphoreRepro.Tests
             };
             t.Start();
 
-            for (int i = 0; i < loops; i++)
+            try
             {
-                Console.WriteLine("[1][{0}] Waiting {1}...", globalSw.Elapsed.TotalMilliseconds, i);
-                var timer = Stopwatch.StartNew();
-                isWaiting = true;
-                await semaphore.WaitAsync(-1);
-                isWaiting = false;
-                timer.Stop();
-                Console.WriteLine("[1][{0}] Waited for {1}...", globalSw.Elapsed.TotalMilliseconds, timer.Elapsed.TotalMilliseconds);
+                for (int i = 0; i < loops; i++)
+                {
+                    output.WriteLine("[1][{0}] Waiting {1}...", globalSw.Elapsed.TotalMilliseconds, i);
+                    var timer = Stopwatch.StartNew();
+                    isWaiting = true;
+                    await semaphore.WaitAsync(-1);
+                    isWaiting = false;
+                    timer.Stop();
+                    output.WriteLine("[1][{0}] Waited for {1}...", globalSw.Elapsed.TotalMilliseconds, timer.Elapsed.TotalMilliseconds);
 
-                if (i <= 0)
-                    continue;
+                    if (i <= 0)
+                        continue;
 
-                // check for too much elapsed time with a little fudge
-                Assert.True(
-                    timer.Elapsed.TotalMilliseconds <= msMax,
-                    "Rate gate took too long to wait in between calls: " + timer.Elapsed.TotalMilliseconds + "ms"
-                );
-                Assert.True(
-                    timer.Elapsed.TotalMilliseconds >= msMin,
-                    "Rate gate took too little to wait in between calls: " + timer.Elapsed.TotalMilliseconds + "ms"
-                );
+                    // check for too much elapsed time with a little fudge
+                    Assert.True(
+                        timer.Elapsed.TotalMilliseconds <= msMax,
+                        "Rate gate took too long to wait in between calls: " + timer.Elapsed.TotalMilliseconds + "ms"
+                    );
+                    Assert.True(
+                        timer.Elapsed.TotalMilliseconds >= msMin,
+                        "Rate gate took too little to wait in between calls: " + timer.Elapsed.TotalMilliseconds + "ms"
+                    );
+                }
+            }
+            finally
+            {
+                isFinished = true;
             }
         }
 
@@ -79,24 +99,30 @@ namespace SemaphoreRepro.Tests
             const double msMax = (double)ms * 1.5;
             const double msMin = (double)ms * (1.0 / 1.5);
 
-            Console.WriteLine("# Semaphore");
-            var semaphore = new Semaphore(timesPerPeriod, timesPerPeriod);
+            output.WriteLine("# Semaphore");
+            using var semaphore = new Semaphore(timesPerPeriod, timesPerPeriod);
             var entered = semaphore.WaitOne(0);
             Assert.True(entered);
             var globalSw = Stopwatch.StartNew();
             var isWaiting = false;
+            var isFinished = false;
 
             var t = new Thread(_ =>
             {
-                while (true)
+                while (!isFinished)
                 {
-                    Console.WriteLine("[0][{0}] Sleeping. ", globalSw.Elapsed.TotalMilliseconds);
+                    output.WriteLine("[0][{0}] Sleeping. ", globalSw.Elapsed.TotalMilliseconds);
+
+                    var timer = Stopwatch.StartNew();
                     Thread.Sleep(ms);
-                    Console.WriteLine("[0][{0}] Sleep done.", globalSw.Elapsed.TotalMilliseconds);
+                    timer.Stop();
+
+                    output.WriteLine("[0][{0}] Sleeped for {1}ms", globalSw.Elapsed.TotalMilliseconds, timer.Elapsed.TotalMilliseconds);
+
                     if (isWaiting)
                     {
                         semaphore.Release();
-                        Console.WriteLine("[0][{0}] Released.", globalSw.Elapsed.TotalMilliseconds);
+                        output.WriteLine("[0][{0}] Released.", globalSw.Elapsed.TotalMilliseconds);
                     }
                 }
             })
@@ -105,28 +131,35 @@ namespace SemaphoreRepro.Tests
             };
             t.Start();
 
-            for (int i = 0; i < loops; i++)
+            try
             {
-                Console.WriteLine("[1][{0}] Waiting {1}...", globalSw.Elapsed.TotalMilliseconds, i);
-                var timer = Stopwatch.StartNew();
-                isWaiting = true;
-                semaphore.WaitOne(-1);
-                isWaiting = false;
-                timer.Stop();
-                Console.WriteLine("[1][{0}] Waited for {1}...", globalSw.Elapsed.TotalMilliseconds, timer.Elapsed.TotalMilliseconds);
+                for (int i = 0; i < loops; i++)
+                {
+                    output.WriteLine("[1][{0}] Waiting {1}...", globalSw.Elapsed.TotalMilliseconds, i);
+                    var timer = Stopwatch.StartNew();
+                    isWaiting = true;
+                    semaphore.WaitOne(-1);
+                    isWaiting = false;
+                    timer.Stop();
+                    output.WriteLine("[1][{0}] Waited for {1}ms...", globalSw.Elapsed.TotalMilliseconds, timer.Elapsed.TotalMilliseconds);
 
-                if (i <= 0)
-                    continue;
+                    if (i <= 0)
+                        continue;
 
-                // check for too much elapsed time with a little fudge
-                Assert.True(
-                    timer.Elapsed.TotalMilliseconds <= msMax,
-                    "Rate gate took too long to wait in between calls: " + timer.Elapsed.TotalMilliseconds + "ms"
-                );
-                Assert.True(
-                    timer.Elapsed.TotalMilliseconds >= msMin,
-                    "Rate gate took too little to wait in between calls: " + timer.Elapsed.TotalMilliseconds + "ms"
-                );
+                    // check for too much elapsed time with a little fudge
+                    Assert.True(
+                        timer.Elapsed.TotalMilliseconds <= msMax,
+                        "Rate gate took too long to wait in between calls: " + timer.Elapsed.TotalMilliseconds + "ms"
+                    );
+                    Assert.True(
+                        timer.Elapsed.TotalMilliseconds >= msMin,
+                        "Rate gate took too little to wait in between calls: " + timer.Elapsed.TotalMilliseconds + "ms"
+                    );
+                }
+            }
+            finally
+            {
+                isFinished = true;
             }
         }
     }
